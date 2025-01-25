0f6555d4fcddc061eb706d0e7745a0773f53bb00
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2025-01-24 21:45:19 -0800

    deleting the readme again lol

--- README.md
+++
@@ -1,5 +0,0 @@
-# cse160-asgn1
-heavily referenced video playlist.
-
-Awesomeness:
-I added a garden brushes. So there is a flower and grass brush to use. The grass is always green and draws straight downwards. The flower has four petals, and you can change the colors of the petals, but the center is always yellow.
--- src/asgn1.js
+++ src/asgn1.js
@@ -4,6 +4,15 @@
 // Student email: japalevi@ucsc.edu
 // 
 
+
+// NOTE FOR GRADER:
+// # cse160-asgn1
+// heavily referenced video playlist.
+
+// Awesomeness:
+// I added a garden brushes. So there is a flower and grass brush to use. The grass is always green and draws straight downwards. The flower has four petals, and you can change the colors of the petals, but the center is always yellow.
+
+
 var VSHADER_SOURCE =`
   attribute vec4 a_Position;
   uniform float u_Size;
